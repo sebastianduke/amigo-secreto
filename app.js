d55cee4a4119c7b2babbc70b8fab2dd89e25a449
sebastianduke <sebastian.duque.fernandez182@gmail.com>
2025-03-11 11:13:46 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    let nombre = input.ariaValueMax.trim();
+    if (nombre === ''){
+        alert("Por favor, ingresaun nombre valido.");
+    }
+}
